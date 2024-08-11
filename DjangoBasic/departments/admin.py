6de5443675b9c89da961c1a80c9b8cc163460bd4
TsaZan <87821236+TsaZan@users.noreply.github.com>
2024-08-11 22:16:18 +0300

    views nad urls

--- DjangoBasic/departments/admin.py
+++ DjangoBasic/departments/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from DjangoBasic.departments.models import Employees
+
+
 # Register your models here.
+@admin.register(Employees)
+class EmployeeAdmin(admin.ModelAdmin):
+    pass
--- DjangoBasic/departments/views.py
+++ DjangoBasic/departments/views.py
@@ -1,15 +1,37 @@
 import time
-
 from django.http import HttpResponse
+from django.shortcuts import render
+
+from DjangoBasic.departments.models import Employees
 
 
 # Create your views here.
-def index(request):
-    return HttpResponse(f"Response: {time.time()} ")
+def index(request, *args, **kwargs):
+    content = (f"Response: {time.time()}</br>"
+               f"User = {request.user}")
+    return HttpResponse(content)
 
 
 def department_by_id(request, pk):
-    return HttpResponse(f"Department ID: {pk}")
+    content = f"Department ID: {pk}"
+    return HttpResponse(content)
+
 
 def department_by_name(request, name):
-    return HttpResponse(f"Department Name: {name}")
+    content = f"Department Name: {name}"
+    return HttpResponse(content)
+
+
+def show_departments_and_ids(request, employee_id):
+
+    employee = Employees.objects.all().filter(pk=employee_id).first()
+
+    content = {"department_name": employee.department_name,
+               "position": employee.position,
+               "employees_first_name": employee.first_name,
+               "employees_last_name": employee.last_name,
+               "logged": request.user,
+               "id": employee.pk}
+    return render(request=request,
+                  template_name="show_departments_and_ids.html",
+                  context=content)
